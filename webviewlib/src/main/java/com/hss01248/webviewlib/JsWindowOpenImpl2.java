02bd61016a4b9267ef24536d36077d35fba2ae96
hss01248 <hss012489@gmail.com>
2020-10-30 22:28:32 +0800

    初步完成window open

--- webviewlib/src/main/java/com/hss01248/webviewlib/JsWindowOpenImpl2.java
+++ webviewlib/src/main/java/com/hss01248/webviewlib/JsWindowOpenImpl2.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+
 import android.webkit.WebView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,36 +22,37 @@ import com.hss01248.activityresult.StartActivityUtil;
 import com.hss01248.activityresult.TheActivityListener;
 import com.just.agentweb.AgentWeb;
 import com.just.agentweb.MiddlewareWebChromeBase;
+import com.just.agentweb.WebChromeClient;
 
 import uk.co.alt236.webviewdebug.DebugWebViewClientLogger;
 
 
-public class JsWindowOpenImpl2 extends MiddlewareWebChromeBase {
+public class JsWindowOpenImpl2 extends WebChromeClient {
 
     volatile WebView newWebView;
-    IWebViewInit webViewInit;
+  public   IWebViewInit webViewInit;
    volatile DefaultWebActivity fullScreenActivity;
 
     public JsWindowOpenImpl2(IWebViewInit webViewInit) {
         this.webViewInit = webViewInit;
     }
 
+
+
     @Override
     public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
         try {
-
-
-            newWebView = new WebView(view.getContext());
-            Log.w("webdebug", "onCreateWindow:isDialog:" + isDialog + ",isUserGesture:" + isUserGesture + ",msg:" + resultMsg + "\n chromeclient:" + this+","+newWebView);
-            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
-            transport.setWebView(newWebView);
-            resultMsg.sendToTarget();
-
             new Handler().post(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        View view1 = initView(view.getContext(), newWebView);
+                        newWebView = new WebView(view.getContext());
+                        WebviewHolder holder = initView(view.getContext(), newWebView);
+                        Log.w("webdebug", "onCreateWindow:isDialog:" + isDialog + ",isUserGesture:" + isUserGesture + ",msg:" + resultMsg + "\n chromeclient:" + this+","+newWebView);
+                        WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+                        transport.setWebView(newWebView);
+                        resultMsg.sendToTarget();
+
                         Activity activity = getActivityFromContext(view.getContext());
                         if(activity instanceof  AppCompatActivity){
                             AppCompatActivity compatActivity = (AppCompatActivity) activity;
@@ -61,8 +63,11 @@ public class JsWindowOpenImpl2 extends MiddlewareWebChromeBase {
                                         protected void onActivityCreated(@NonNull DefaultWebActivity activity, @Nullable Bundle savedInstanceState) {
                                             super.onActivityCreated(activity, savedInstanceState);
                                             //fullScreenActivity = activity;
-                                            activity.setContent(view1);
-                                            webViewInit.getImpl().fullScreenActivity = activity;
+                                            activity.setContent(holder.root);
+                                            activity.agentWeb = holder.agentWeb;
+                                            activity.holder = holder;
+                                            holder.activity = activity;
+                                            holder.impl2.fullScreenActivity = activity;
 
 
                                         }
@@ -109,7 +114,7 @@ public class JsWindowOpenImpl2 extends MiddlewareWebChromeBase {
         return null;
     }
 
-    private View initView(Context context, WebView webView) {
+    private WebviewHolder initView(Context context, WebView webView) {
         Activity activity = getActivityFromContext(context);
         WebviewHolder holder = new WebviewHolder(activity);
         //配置
@@ -125,7 +130,7 @@ public class JsWindowOpenImpl2 extends MiddlewareWebChromeBase {
             }
         });
         holder.loadUrl("");
-        return holder.root;
+        return holder;
     }
 
 
